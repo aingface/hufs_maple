4369731a215ecddc5e42fd76985b368e535d0bb4
Daecheol <qoeocjf5@gmail.com>
2022-06-14 18:32:43 +0900

    Fix: Image태그 사용시에도 overay 되도록코드 수정

--- components/CarouselChart.tsx
+++ components/CarouselChart.tsx
@@ -4,7 +4,7 @@ import reverse_city from '/public/images/reverse_city.jpeg'
 import eurel from '/public/images/eurel.jpeg'
 import florida_beach from '/public/images/florida_beach.jpeg'
 import styled from 'styled-components';
-
+import styles from '/styles/Home.module.css'
 
 const images=[ reverse_city, eurel, florida_beach ]
 // const images=[ 
@@ -61,50 +61,49 @@ const CarouselChart = () => {
     setTransToR(false)
   } 
 
-
-  //Image 태그를 사용하면 이미지가 2개가 뜨는가? 이걸 해결해야함. 
-  //슬라이드 원리는 현재,다음 이미지 2개를 연달아 붙여서 현재 이미지는 0->-100%,
-  //다음 이미지는 100% -> 0 보여주는 것이다. 이를 잘 생각해보자. 
   return (
     <>
     <div className='flex justify-center space-x-4 mt-16'>
       <button className='h-auto w-10 bg-red-300 font-extrabold text-3xl' onClick={handlePrev}>{'<'}</button>
-        <div className='relative flex w-96 border-2 h-58 overflow-hidden rounded-xl'>
+        <div className={styles.overlapGrid}
+        style={{
+          width: '50vw',
+          height: '30vh',
+        }}
+        >
           {/* 현재 이미지 */}
           <Image 
-            className={ `absolute object-contain overflow-visible z-20 w-full h-full 
+            className={ `
+            object-contain 
+            z-20 
+            w-full h-full 
+            border-solid border-4 border-red-500
               ${ transToL ? 'transition duration-500 ease-linear transform -translate-x-full' :
               (transToR ? 'animate-slideFromL' : '')}`}
             src={images[index]} 
+            layout='responsive'
+            objectFit='cover'
             alt=""
           />  
           {/* 이전 or 이후 이미지 */}
           <Image 
-            className={`absolute object-contain overflow-visible z-0 w-full h-full
+            className={`
+            object-contain 
+            z-0 
+            w-full 
+            h-full
+            layout='responsive'
+            
             ${transToL ? 'animate-slideFromR' : 
             (transToR ? 'transition duration-500 ease-linear transform translate-x-full': '')}`} 
-            src={images[index1]} 
+            src={images[index1]}
+            layout='responsive'
+            objectFit='cover'
             alt=""
           />  
         </div>
       <button className='h-100 w-10 bg-red-300 font-extrabold text-3xl' onClick={handleNext}>{'>'}</button>
     </div>
-    {/* <Image  
-      className={ `absolute object-contain z-20 w-full h-full  
-      ${ transToL ? 'transition duration-500 ease-linear transform -translate-y-full' :
-        (transToR ? 'animate-slideR' : '')}`}
-      src={images[index]} 
-      alt=""
-    />  
-    {/* 다음 이미지 */}
-    {/*
-    <Image 
-      className={`absolute object-contain z-0 w-full h-full m-1
-      ${transToL ? 'transition duration-500 ease-linear transform -translate-y-full' : 
-      (transToR ? 'animate-slideL': '')}`} 
-      src={images[index1]} 
-      alt=""
-    />   */}
     </>
   );
 };
